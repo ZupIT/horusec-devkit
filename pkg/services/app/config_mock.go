caccac7a654c7b08ebe3bbd880e9ffd29be64ec3
wilian <wilian.silva@zup.com.br>
2021-04-05 15:37:33 -0300

    Adding config mock

---
+++ pkg/services/app/config_mock.go
@@ -0,0 +1,24 @@
+package app
+
+import (
+	"github.com/stretchr/testify/mock"
+
+	"github.com/ZupIT/horusec-devkit/pkg/enums/auth"
+)
+
+type Mock struct {
+	mock.Mock
+}
+
+func (m *Mock) IsBrokerDisabled() bool {
+	args := m.MethodCalled("IsBrokerDisabled")
+	return args.Get(0).(bool)
+}
+func (m *Mock) IsApplicationAdmEnabled() bool {
+	args := m.MethodCalled("IsApplicationAdmEnabled")
+	return args.Get(0).(bool)
+}
+func (m *Mock) GetAuthorizationType() auth.AuthorizationType {
+	args := m.MethodCalled("GetAuthorizationType")
+	return args.Get(0).(auth.AuthorizationType)
+}
