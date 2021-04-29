a30cb2d98b44634d5850dccd3633a62f64de8322
Wilian Gabriel <63816070+wiliansilvazup@users.noreply.github.com>
2021-04-29 10:31:30 -0300

    Update coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -18,7 +18,7 @@ GO_FILES=$2
 PROJECT=$3
 
 go clean -testcache
-go test -v -coverpkg "$GO_FILES" -coverprofile coverage.out "$GO_FILES"
+go test -v -coverpkg $GO_FILES -coverprofile coverage.out $GO_FILES
 
 sed -i '/mock.go/d' coverage.out
 sed -i '/pb.go/d' coverage.out
