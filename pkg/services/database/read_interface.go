4270df2e6cb7a00c2408085ffc498290eda83931
wilian <wilian.silva@zup.com.br>
2021-05-04 13:11:55 -0300

    Adding findPreload on interface

--- pkg/services/database/read_interface.go
+++ pkg/services/database/read_interface.go
@@ -6,6 +6,8 @@ import (
 
 type IDatabaseRead interface {
 	IsAvailable() bool
+	FindPreload(entityPointer interface{}, where map[string]interface{}, preloads []string,
+		table string) response.IResponse
 	Find(entityPointer interface{}, where map[string]interface{}, table string) response.IResponse
 	First(entityPointer interface{}, where map[string]interface{}, table string) response.IResponse
 	Raw(rawSQL string, entityPointer interface{}, values ...interface{}) response.IResponse
