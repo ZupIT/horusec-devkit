4f0f347e1f4ebc7f7e4721c943af737866d82b3b
Wilian Gabriel <wilian.silva@zup.com.br>
2022-02-22 16:40:34 -0300

    mageutils:feat - Adding new method to add/remove tag (#166)
    In this commit I added the new method CreateAndPushTag for create and
    up new tag. And other new method RemoveTag for delete local and in
    origin tag.
    
    Signed-off-by: Wilian Gabriel <wilian.silva@zup.com.br>

--- pkg/utils/mageutils/release.go
+++ pkg/utils/mageutils/release.go
@@ -44,6 +44,32 @@ func CreateLocalTag(tag string) (err error) {
 	return sh.RunV("git", "tag", "-s", tag, "-m", "release "+tag)
 }
 
+// CreateAndPushTag create and push a new given tag executing
+// "git tag -s tag -m release+tag" and "git push --tags"
+func CreateAndPushTag(tag string) (err error) {
+	mg.Deps(isGitExistent)
+	mg.Deps(mg.F(isValidTag, tag))
+
+	if err := sh.RunV("git", "tag", "-s", tag, "-m", "release "+tag); err != nil {
+		return err
+	}
+
+	return sh.RunV("git", "push", "--tags")
+}
+
+// RemoveTag remove tag locally and in the origin
+// "git tag -d tag" and "git push --delete origin tag"
+func RemoveTag(tag string) (err error) {
+	mg.Deps(isGitExistent)
+	mg.Deps(mg.F(isValidTag, tag))
+
+	if err := sh.RunV("git", "tag", "-d", tag); err != nil {
+		return err
+	}
+
+	return sh.RunV("git", "push", "--delete", "origin", tag)
+}
+
 // CheckoutReleaseBranch creates if not exists a release branch and then checkout
 // @TODO validate release branch name with regex
 func CheckoutReleaseBranch(branchName string) error {
